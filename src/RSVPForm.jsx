733eba4cf0fc44cc27b2f66d1ea4d7a8fda1894e
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-04-12 18:43:34 -0500

    submitted solution

--- src/RSVPForm.jsx
+++ src/RSVPForm.jsx
@@ -1,5 +1,87 @@
 import React, { useState } from "react";
 
-function RSVPForm() {}
+function RSVPForm() {
+  const initialFormState = {
+    name: "",
+    age: "",
+    newMember: false,
+    comment: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormState);
+
+  const handleChange = ({ target }) => {
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    setFormData({ ...formData, [target.name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData.name, formData.age, formData.newMember, formData.comment);
+    setFormData({ ...initialFormState });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">
+        Enter Your Name:
+        <input
+          id="name"
+          type="text"
+          name="name"
+          onChange={handleChange}
+          // value={formData.name}
+        />
+      </label>
+
+      <br />
+
+      <label htmlFor="age">
+        How did you hear about us?
+        <select
+          id="age"
+          name="age"
+          onChange={handleChange}
+          // value={formData.age}
+        >
+          <option value="">Prefer not to say</option>
+          <option value="0-19">0-19</option>
+          <option value="20-39">20-39</option>
+          <option value="40-59">40-59</option>
+          <option value="60+">60+</option>
+        </select>
+      </label>
+
+      <br />
+
+      <label htmlFor="newMember">
+        New Member?
+        <input
+          id="newMember"
+          type="checkbox"
+          name="newMember"
+          onChange={handleChange}
+          checked={formData.newMember}
+          value="newMember"
+        />
+      </label>
+
+      <br />
+
+      <label htmlFor="comment">
+        Any Comments?:
+        <input
+          id="comment"
+          type="text"
+          name="comment"
+          onChange={handleChange}
+          // value={formData.comment}
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 
 export default RSVPForm;
